513cebdbf5211a0f02fbda84758a9513d1e6a6b9
willianmv <willianvmarques300@gmail.com>
2025-05-11 14:01:12 -0300

    chore: configura a estrutura inicial do projeto

---
+++ src/test/java/com/example/email_validation/EmailValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.email_validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
