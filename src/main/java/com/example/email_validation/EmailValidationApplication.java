a275cb4837417ad9ec26e6a3c9b4aa80a1f1bb8a
willianmv <willianvmarques300@gmail.com>
2025-05-11 14:40:05 -0300

    feat(auditioning): habilita auditoria JPA na aplicação

--- src/main/java/com/example/email_validation/EmailValidationApplication.java
+++ src/main/java/com/example/email_validation/EmailValidationApplication.java
@@ -2,8 +2,10 @@ package com.example.email_validation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EmailValidationApplication {
 
 	public static void main(String[] args) {
