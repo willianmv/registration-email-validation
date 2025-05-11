df05b7bed3e14eabc5ff4a6292ce0c0b6c32c851
willianmv <willianvmarques300@gmail.com>
2025-05-11 17:15:49 -0300

    fix(auth): corrige dto para registro de usuário

--- src/main/java/com/example/email_validation/auth/RegistrationRequestDto.java
+++ src/main/java/com/example/email_validation/auth/RegistrationRequestDto.java
@@ -5,31 +5,25 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter
-@Setter
 @Builder
-public class RegistrationRequest {
+public record RegistrationRequestDto(
+        @NotEmpty(message = "Firstname is required")
+        @NotBlank(message = "Firstname is required")
+        String firstName,
 
-    @NotEmpty(message = "Firstname is required")
-    @NotBlank(message = "Firstname is required")
-    private String firstName;
+        @NotEmpty(message = "Lastname is required")
+        @NotBlank(message = "Lastname is required")
+        String lasName,
 
-    @NotEmpty(message = "Lastname is required")
-    @NotBlank(message = "Lastname is required")
-    private String lasName;
+        @NotEmpty(message = "Email is required")
+        @NotBlank(message = "Email is required")
+        @Email(message = "Email is not formatted") String email,
 
-    @NotEmpty(message = "Email is required")
-    @NotBlank(message = "Email is required")
-    @Email(message = "Email is not formatted")
-    private String email;
-
-    @NotEmpty(message = "Password is required")
-    @NotBlank(message = "Password is required")
-    @Size(min = 8, message = "Password must have 8 characters long minimum")
-    private String password;
-
-
+        @NotEmpty(message = "Password is required")
+        @NotBlank(message = "Password is required")
+        @Size(min = 8, message = "Password must have 8 characters long minimum")
+        String password
+)
+{
 }
