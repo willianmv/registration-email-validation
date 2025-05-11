98fa72eb77d3bac11919c7babc6d28f589124b51
willianmv <willianvmarques300@gmail.com>
2025-05-11 16:22:39 -0300

    feat(security): implementa busca pelo usuário na base de dados

---
+++ src/main/java/com/example/email_validation/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.example.email_validation.security;
+
+import com.example.email_validation.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    @Override
+    public UserDetails loadUserByUsername(String userEmail) throws UsernameNotFoundException {
+        return userRepository.findByEmail(userEmail)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
