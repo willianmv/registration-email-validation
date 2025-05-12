b414a6313931c55802e8dff260c283f2efc00d14
willianmv <willianvmarques300@gmail.com>
2025-05-11 21:41:19 -0300

    feat(mail-service): implementa suporte a método assíncrono e cria role user ao subir a aplicação

--- src/main/java/com/example/email_validation/EmailValidationApplication.java
+++ src/main/java/com/example/email_validation/EmailValidationApplication.java
@@ -1,15 +1,32 @@
 package com.example.email_validation;
 
+import com.example.email_validation.role.Role;
+import com.example.email_validation.role.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableAsync
 public class EmailValidationApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmailValidationApplication.class, args);
 	}
+
+	@Bean
+	public CommandLineRunner runner(RoleRepository roleRepository){
+		return args -> {
+			if(roleRepository.findByName("USER").isEmpty()){
+				roleRepository.save(
+						Role.builder().name("USER").build());
+			}
+		};
+	}
+
 
 }
