9b660f97e068fd145cb438285fe55f088e270234
willianmv <willianvmarques300@gmail.com>
2025-05-11 21:47:31 -0300

    feat(mail-service): implementa confgiração e envio de email

---
+++ src/main/java/com/example/email_validation/email/EmailService.java
@@ -0,0 +1,68 @@
+package com.example.email_validation.email;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.springframework.mail.javamail.MimeMessageHelper.*;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    private final SpringTemplateEngine templateEngine;
+
+    @Async
+    public void sendEmail(
+            String to,
+            String username,
+            EmailTemplate emailTemplate,
+            String confirmationUrl,
+            String activationCode,
+            String subject) throws MessagingException {
+
+        String templateName;
+        if(emailTemplate == null){
+            templateName = "confirm-email";
+        }else {
+            templateName = emailTemplate.name();
+        }
+
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(
+                mimeMessage, MULTIPART_MODE_MIXED, UTF_8.name());
+
+        Map<String, Object> properties = new HashMap<>();
+        properties.put("username", username);
+        properties.put("confirmationUrl", confirmationUrl);
+        properties.put("activation_code", activationCode);
+
+        Context context = new Context();
+        context.setVariables(properties);
+
+        mimeMessageHelper.setFrom("contact@test.com");
+        mimeMessageHelper.setTo(to);
+        mimeMessageHelper.setSubject(subject);
+
+        String template = templateEngine.process(templateName, context);
+
+        mimeMessageHelper.setText(template, true);
+
+        mailSender.send(mimeMessage);
+    }
+
+}
---
+++ src/main/java/com/example/email_validation/email/EmailTemplate.java
@@ -0,0 +1,15 @@
+package com.example.email_validation.email;
+
+import lombok.Getter;
+
+@Getter
+public enum EmailTemplate {
+
+    ACTIVATE_ACCOUNT("activate_account");
+
+    private final String name;
+
+    EmailTemplate(String name) {
+        this.name = name;
+    }
+}
